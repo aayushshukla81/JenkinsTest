f097e5c589101ba7d937934a865660c214415b0d
aayush shukla <ayaush.shukla@acxiomconsulting.com>
2022-02-23 14:05:53 +0530

    Add project files.

---
+++ JenkinsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace JenkinsTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, this is a jenkins test project");
+        }
+    }
+}
+
